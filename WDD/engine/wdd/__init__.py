9f8ac29662363f9e57281b82e96e7928869b4c24
HaipChen <1036911544@qq.com>
2026-09-08 19:26:33 +0800

    Delete WDD directory

--- WDD/engine/wdd/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .wdd import WDD
-
-from .wdd_encoder import CFDFEncoder
-
-from .dfine_decoder import DFINETransformer
-from .wdd_criterion import WDDCriterion
-from .wdd_decoder import WDDTransformer
